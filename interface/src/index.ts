9a532d70a393d3d7d7b53830bb792a2bfa4e20b8
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-05 23:49:00 +0530

    feat: interface added

--- interface/src/index.ts
+++ interface/src/index.ts
@@ -1,2 +1,50 @@
-const city: string = "pune"
-console.log(city);
+// const isLegal = (user: {
+//     name: string,
+//     email: string,
+//     age: number
+// }) => {
+//     if (user.age > 18){
+//         return true
+//     } else {
+//         return false
+//     }
+// };
+// const result = isLegal({
+//     name: "john",
+//     email: "john@gmail.com",
+//     age: 20
+// });
+// console.log(result);
+
+//using interface
+interface User {
+    name: string,
+    email: string,
+    age: number,
+};
+
+const isLegal = (user: User) => {
+    if(user.age > 18){
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const greet = (user: User) => {
+    console.log(`Hello ${user.name} you are ${user.age} years old`);
+    
+}
+
+const result = isLegal({
+    name: "john",
+    email: "john@gmail.com",
+    age: 20
+});
+console.log(result);
+
+greet({
+    name: "john",
+    email: "john@gmail.com",
+    age: 20
+});
