61cdcae413b74958f087f68e037999e3ba92998d
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-23 16:20:39 +0530

    feat: type-annotations added

---
+++ source-code/src/Type-Annotation/1.ts
@@ -0,0 +1,23 @@
+//type annotation
+
+let text: string = "coding"
+
+text = "coding again"
+text = text.toUpperCase();
+
+// text = 69;
+
+console.log(text);
+
+let amount: number = 69;
+amount = 69 - 69;
+// amount = "string"
+
+let isCoding: boolean = true;
+isCoding = false;
+// isCoding = 69;
+
+//type inference
+let payment = 69000;
+// payment = "sixty-nine-thousand";
+
