7ddeaf535784643395f20ffe29a7f9e436f9afad
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-23 16:29:25 +0530

    feat: union types added

---
+++ source-code/src/Union-Type/1.ts
@@ -0,0 +1,6 @@
+//union types allow us to set up multiple types
+
+let tax: number | string;
+
+tax = 69;
+tax = "sixty-nine";
---
+++ source-code/src/Union-Type/2.ts
@@ -0,0 +1,9 @@
+//literal type
+
+let requestStatus: 'complete' | 'inprogress' | 'yet to start';
+
+//it gives suggestions
+requestStatus = 'inprogress'
+
+//it gives error
+// requestStatus = "error"
