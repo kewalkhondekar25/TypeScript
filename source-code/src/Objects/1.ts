f11edff984c0efbe226044609f09fe734b401d2f
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-23 17:22:26 +0530

    feat: options added

---
+++ source-code/src/Objects/1.ts
@@ -0,0 +1,20 @@
+let car: {brand: string; price: number} = {brand: "toyata", price: 699}
+
+car.brand = "bmw";
+// car.price = false
+
+let book = {
+    title: "tintin",
+    cost: 25 
+}
+let pen = {
+    title: "cello",
+    cost: 10
+}
+let notebook = {
+    title: "classmate",
+}
+
+// let items: {title: string; cost: number}[] = [book, pen, notebook]
+let items: {title: string; cost?: number}[] = [book, pen, notebook]
+
