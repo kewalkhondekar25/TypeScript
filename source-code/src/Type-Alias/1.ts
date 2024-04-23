7bdac78665af810ff923ffc2e3f6c60c00becaf1
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-23 23:16:22 +0530

    feat: type alias added

---
+++ source-code/src/Type-Alias/1.ts
@@ -0,0 +1,18 @@
+// type alias
+type User = {id: number; name: string; isPass: boolean};
+
+const john: User = {
+    id: 22,
+    name: "john",
+    isPass: true
+};
+const susan: User = {
+    id: 69,
+    name: "susan",
+    isPass: true
+};
+
+const showResult = (user: User): User => {
+        console.log(`Hi ${user.name} you are ${user.isPass ? "Passed":"Failed"}`);
+        return user
+};
