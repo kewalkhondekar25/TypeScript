ff4e67e8c1de862b5cef492041377605c1b26a72
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-25 00:43:37 +0530

    feat: ts generics added

---
+++ source-code/src/Generics/1.ts
@@ -0,0 +1,3 @@
+// In TypeScript, generics provide a way to create reusable components that can work with a variety of data     types while maintaining type safety.
+
+let menu: Array<string> = ["idli", "dosa", "poha"];
