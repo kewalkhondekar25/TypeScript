e01a1b497cfca2cdf1024a6de92f1b50b55f3f1b
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-24 16:43:34 +0530

    feat: tuples added

---
+++ source-code/src/Tuples/1.ts
@@ -0,0 +1,11 @@
+let person: [string, number] = ["john", 69];
+let date: [number, number] = [20, 2024];
+
+// date.push(33);
+// date.push("asd")
+
+const getPerson = (): [string, number] => {
+    return ["john", 69]
+}
+
+let food: [string, number?] = ["biryani"];
