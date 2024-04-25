197f2d92bd2d1ee146907d87d40b655726e4715f
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-25 15:07:02 +0530

    feat: async fetch method added

--- source-code/src/Fetch-Data/1.ts
+++ source-code/src/Fetch-Data/1.ts
@@ -1,6 +1,15 @@
-const url = "https://fakestoreapi.com/products"
+const url = "https://vegan-meals-api.vercel.app/api/v1/meals"
 
-const fetchData = async (url: string): Promise<void> => {
+interface Products {
+    _id: number;
+    category: string;
+    title: string;
+    description: string;
+    price: number;
+    __v: number
+}
+
+const fetchData = async (url: string): Promise<Products[]> => {
     try {
         const response = await fetch(url);
         if(!response.ok){
@@ -8,10 +17,15 @@ const fetchData = async (url: string): Promise<void> => {
         };
         const data = await response.json();
         console.log(data);
+        return data;
     } catch (error) {
-        const errorMsg = error instanceof Error? error.message : "there is an error"
+        const errorMsg = error instanceof Error? error.message : "there is an error";
         console.log(errorMsg);
+        return [];
     }
 };
 
-fetchData(url);
+const results = await fetchData(url);
+results.map(item => console.log(item.price));
+
+export {results};
