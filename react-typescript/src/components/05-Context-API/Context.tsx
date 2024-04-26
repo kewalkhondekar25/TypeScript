33ffd628603cb465b9482ba33be24ce1ef6da0d0
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-27 00:41:46 +0530

    feat: context api theme added

--- react-typescript/src/components/05-Context-API/Context.tsx
+++ react-typescript/src/components/05-Context-API/Context.tsx
@@ -1,11 +1,26 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
+
+type Theme = "dark" | "light" | "system";
+type ThemeState = {
+    theme: string,
+    setTheme: (theme: Theme) => void
+}
+type ThemeProps = {
+    children: React.ReactNode,
+    defaultTheme?: Theme
+}
 
 //create context
-const AppContext = createContext<string | undefined>(undefined);
+const AppContext = createContext<ThemeState | undefined>(undefined);
+
 
 //create provider
-const AppProvider = ({children}: {children: React.ReactNode}) => {
-  return <AppContext.Provider value="hello world">
+const AppProvider = ({children, defaultTheme = "system"}:ThemeProps) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);  
+  return <AppContext.Provider value={{
+    theme,
+    setTheme
+  }}>
     {children}
   </AppContext.Provider>
 }
