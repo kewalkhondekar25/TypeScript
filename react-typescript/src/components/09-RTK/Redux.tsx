a3e441b1940344571ba27b275d2db12dd11130e6
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-28 02:41:02 +0530

    feat: redux-tool-kit types implemented

--- react-typescript/src/components/09-RTK/Redux.tsx
+++ react-typescript/src/components/09-RTK/Redux.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
+import { useAppSelector, useAppDispatch } from './hooks'
+import { increment, decrement, reset, setStatus } from './features/counterSlice'
 
 const Redux = () => {
+  
+  const {count, status} = useAppSelector(state => state.counter);
+  const dispatch = useAppDispatch();
   return (
     <div>
       <h3>Redux Tool Kit</h3>
       <div>
-        <h4>Count: 0</h4>
-        <h5>Status: pending</h5>
+        <h4>Count: {count}</h4>
+        <h5>Status: {status}</h5>
         <div className='container'>
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
-          <button>Set Status Active</button>
-          <button>Set Status Inactive</button>
+          <button
+            onClick={() => dispatch(increment())}>Increment</button>
+          <button
+            onClick={() => dispatch(decrement())}>Decrement</button>
+          <button
+            onClick={() => dispatch(reset())}>Reset</button>
+          <button
+            onClick={() => dispatch(setStatus("active"))}>Set Status Active</button>
+          <button
+            onClick={() => dispatch(setStatus("inactive"))}>Set Status Inactive</button>
         </div>
       </div>
     </div>
