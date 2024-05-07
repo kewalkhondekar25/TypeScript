031b252a598b1a10368e70e848fd0cb0afb3170d
keal khondekar <kewalkhondekar25@gmail.com>
2024-05-07 19:01:42 +0530

    feat: jwt sign & verify added

--- nodejs-typescript/authentication/src/controller/controller.ts
+++ nodejs-typescript/authentication/src/controller/controller.ts
@@ -10,8 +10,25 @@ const login = asyncHandler(async (req: express.Request, res: express.Response, n
   if(!(userName && password)){
     return res.status(404).json({message: "can not be empty"})
   }
-  const token = jwt.sign({id, userName}, process.env.JWT_SECRET, {expiresIn: "30d"})
+  const token = jwt.sign({id, userName}, process.env.JWT_SECRET, {expiresIn: "30d"});
   return res.status(200).json({ message: "user created", token })
+});
+
+const dashboard = asyncHandler(async(req: express.Request, res: express.Response) => {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  
+  
+  if(!authHeader || !authHeader.startsWith("Bearer ")){
+    return res.status(401).json({message: "no token provided"})
+  };
+  try {
+    const token = authHeader.split(" ")[1]
+    const decode = jwt.verify(token, process.env.JWT_SECRET)
+    return res.status(200).json({mesaage: "welcome to dashboard"})
+  } catch (error) {
+    res.status(401).json({message: "unathorized"})
+  }
 })
 
-export { login };
+export { login, dashboard };
--- nodejs-typescript/authentication/src/routes/routes.ts
+++ nodejs-typescript/authentication/src/routes/routes.ts
@@ -1,8 +1,9 @@
-import { login } from "../controller/controller"
+import { dashboard, login } from "../controller/controller"
 import {Router} from "express"
 
 const router = Router();
 
-router.route("/login").post(login)
+router.route("/login").post(login);
+router.route("/dashboard").get(dashboard);
 
 export default router;
