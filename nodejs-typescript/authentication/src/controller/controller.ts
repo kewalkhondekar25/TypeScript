a657d01c85eedf21518fb80c682dd6ecf507e27e
keal khondekar <kewalkhondekar25@gmail.com>
2024-05-07 14:47:08 +0530

    feat: jwt added

--- nodejs-typescript/authentication/src/controller/controller.ts
+++ nodejs-typescript/authentication/src/controller/controller.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import asyncHandler from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+
 const login = asyncHandler(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
 
   const {userName, password} = req.body;
-  console.log(`${userName} & ${password}`);
-  
+  const id = new Date().getDate();
+
   if(!(userName && password)){
     return res.status(404).json({message: "can not be empty"})
   }
-  
-  return res.status(200).json({ message: "userName" })
+  const token = jwt.sign({id, userName}, process.env.JWT_SECRET, {expiresIn: "30d"})
+  return res.status(200).json({ message: "user created", token })
 })
 
 export { login };
